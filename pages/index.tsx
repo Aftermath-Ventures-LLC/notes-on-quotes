fdce638b12d3b60cc462a2d377e03f3d279d70cd
Brian Wang <brrian@gmail.com>
2019-11-29 13:30:00 -0600

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Notes on Quotes
+      <p>
+        <Link href="/about">About</Link>
+      </p>
+    </div>
+  )
+}
+
+export default Home
