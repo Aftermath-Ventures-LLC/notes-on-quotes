51c62f30c8f86b9c445b01a24b0f827c237882aa
Brian Wang <brrian@gmail.com>
2019-12-03 21:01:31 -0600

    Add linters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const Home = () => {
   return (
@@ -9,7 +9,7 @@ const Home = () => {
         <Link href="/about">About</Link>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
