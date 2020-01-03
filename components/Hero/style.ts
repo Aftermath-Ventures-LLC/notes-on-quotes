b03dbe07014660e495774c547f56ff946662df58
Brian Wang <brrian@gmail.com>
2020-01-02 18:45:32 -0600

    Add logo support for home page

--- components/Hero/style.ts
+++ components/Hero/style.ts
@@ -3,10 +3,20 @@ import Container from '../../elements/Container';
 import { space, fontSize } from '../../styles/theme';
 
 export const HeroContainer = styled(Container)({
+  alignItems: 'center',
   marginTop: space(5),
   marginBottom: space(5),
   textAlign: 'center',
+});
 
+export const Logo = styled.img({
+  width: 450,
+  maxWidth: '80%',
+  marginTop: space(3),
+  marginBottom: space(3),
+});
+
+export const Content = styled.div({
   h1: {
     fontSize: fontSize(4),
   },
@@ -17,5 +27,9 @@ export const HeroContainer = styled(Container)({
 
   p: {
     lineHeight: 1.4,
+  },
+
+  img: {
+    maxWidth: '100%',
   },
 });
